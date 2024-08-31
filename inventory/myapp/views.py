2c7b86f84ca0ab05e45b75391bb64abb4fdcd424
Tarikul69 <tarikulabir69@gmail.com>
2024-08-31 23:09:26 +0600

    Update

--- inventory/myapp/views.py
+++ inventory/myapp/views.py
@@ -7,5 +7,26 @@ from django.contrib import messages
 def login(request):
     return render(request, 'login/login.html')
 
-def custom_login(request):
-    pass
+def login_function(request):
+    print("Good")
+    if request.method == 'POST':
+        print("Good-1")
+        username = request.POST['username']
+        print(username)
+        password = request.POST['password']
+        print(password)
+        user = authenticate(username, password)
+        print(user)
+
+        if user is not None:
+            login(request, user)
+            print("Good")
+            return render(request, 'supper_admin/home.html')
+        
+        else:
+            messages.error(request, "Wrong Credentials")
+            return redirect('')
+           
+
+    return render(request, 'login/login.html')
+
