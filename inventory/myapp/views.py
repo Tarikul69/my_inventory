a11ee6a9b43a551fb1c6a0ca92410cb46b6ad302
Tarikul69 <tarikulabir69@gmail.com>
2024-08-31 18:18:04 +0600

    Update

--- inventory/myapp/views.py
+++ inventory/myapp/views.py
@@ -8,21 +8,4 @@ def login(request):
     return render(request, 'login/login.html')
 
 def custom_login(request):
-    if request.method == 'POST':
-      form = AuthenticationForm(request, data=request.POST)
-      if form.is_valid():
-         username = form.cleaned_data.get('username')
-         password = form.cleaned_data.get('password')
-         user = authenticate(username=username, password=password)
-         if user is not None:
-                login(request, user)
-                messages.success(request, f"Welcome back, {username}!")
-                return redirect('home')  # Redirect to a desired page after successful login
-         else:
-            messages.error(request, "Invalid username or password.")
-      else:
-        messages.error(request, "Invalid username or password.")
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'login.html', {'form': form})
+    pass
