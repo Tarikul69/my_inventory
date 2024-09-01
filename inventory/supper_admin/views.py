37292e66d51363c289487d0e22c491eee46f19ce
Tarikul69 <tarikulabir69@gmail.com>
2024-09-02 02:57:17 +0600

    Update

--- inventory/supper_admin/views.py
+++ inventory/supper_admin/views.py
@@ -2,13 +2,16 @@
 from pyexpat.errors import messages
 from django.shortcuts import render
 
+from supper_admin.models import product
+
 
 def supper_admin(request):
     return render(request, 'supper_admin/home.html')
 
 
 def product_list(request):
-    return render(request, 'supper_admin/product_list.html')
+    products = product.objects.all()
+    return render(request, 'supper_admin/product_list.html', {'products': products})
 
 def inventory(request):
     return render(request, 'supper_admin/inventory.html')
