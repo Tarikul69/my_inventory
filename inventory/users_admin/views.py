f7ba44b1cab1b977e6b99945d7b33af9b61258e3
Tarikul69 <tarikulabir69@gmail.com>
2024-09-02 13:29:54 +0600

    Update

--- inventory/users_admin/views.py
+++ inventory/users_admin/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from supper_admin.models import product
+
 # Create your views here.
 def users_admin(request):
     return render(request, 'users_admin/home.html')
 
 def purchage(request):
-    return render(request, 'users_admin/purchage.html')
+    products = product.objects.all()
+    return render(request, 'users_admin/purchage.html', {'products': products})
